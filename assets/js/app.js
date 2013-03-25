00aef97ce2bc358e7c0359f2cac50702fb4a7fef
lb <lb@fake.email>
2013-03-25 04:53:17 -0400

    shows a map
    yay (but lots of test code)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,29 @@
 ;(function($, window) {
 
-	var jQT;
+	var jQT, map;
 	
 	$(document).ready(function() {
 	
 		jQT = $.jQTouch({
 				statusBar: 'black'
 		});
+		
+		//map = $("#map").gmap();
+		
+		function initialize() {
+        var mapOptions = {
+          zoom: 8,
+          center: new google.maps.LatLng(-34.397, 150.644),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+		map = new google.maps.Map($("#map")[0], mapOptions);
+      }
+	  
+	  
+	  initialize();
+		console.log(map);
+      /*google.maps.event.addDomListener(window, 'load', initialize); */
+		
 		console.log("hi");
 	});
 }(jQuery, this));
--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -1,9 +1,31 @@
-;(function($, window) {
+;(function($) {
 
-	var MobileMap = function(obj, options) {
-		var _default = {
-			
+	var gmap = function(obj, options) {
+		var $mapDiv = $(obj);
+	
+		var t = {
+			mapOptions: {
+				zoom: 15,
+				center: new google.maps.LatLng(0, 0), 
+				mapTypeId: google.maps.MapTypeId.ROADMAP, 
+				scrollwheel: false
+			},
+			mapDiv: $mapDiv
 		}
+		
+		t.init = function(options) {
+			t.map = new google.maps.Map(t.mapDiv, t.mapOptions);
+			return t.map;
+		}
+		
+		t.init();
+		console.log(t);
+		return t;
 	}
 	
-}(jQuery, this));
+	//jQuery alias
+	$.fn.gmap = function(options) {
+		return new gmap($(this), options);
+	}
+	
+})(jQuery);
