95716a234ddfdeab07796fc05ad1320b6891eecf
lb <lb@fake.email>
2013-03-25 22:19:52 -0400

    localstorage save markers implemented

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,10 +18,12 @@
 			if (info.direction == 'in') {
 				$("#map").show();				
 				google.maps.event.trigger(map.map, 'resize');
+				map.map.fitBounds(map.bounds);
 			}
 			return false;
 		});
 	
+		//marker form
 		$("#new-marker").submit(function(e) {
 			var $t      = $(this);
 			var $name   = $t.find("#name");
@@ -37,6 +39,14 @@
 				$zip.val()
 			];
 			
+			var resetFields = function() {
+				$name.val("");
+				$street.val("");
+				$city.val("");
+				$state.val("");
+				$zip.val("");
+			}
+			
 			map.geocode(address.join(" "), function(response) {
 				console.log(response);
 				
@@ -44,11 +54,17 @@
 					var lat = response.results[0].geometry.location.lat();
 					var lng = response.results[0].geometry.location.lng();
 					
+					
+					
 					map.addMarker(lat, lng, $name.val());
+					map.saveRow({name: $name.val(), street: $street.val(), city: $city.val(), state: $state.val(), zip: $zip.val(), lat: lat, lng: lng});
+					resetFields();
+					goHome();
+				} else {
+					alert("Invalid address. Enter a different address.");
 				}
 			});
 			
-			goHome();
 			e.preventDefault();
 		});
 
--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -2,27 +2,48 @@
 
 	var gmap = function(obj, options) {
 		var $mapDiv = $(obj);
+		var _DB = "markerDB";
+		var _TABLE = "markerTable";
 	
 		var t = {
 			bounds: new google.maps.LatLngBounds(),
-			//db: new localStorageDB("markerDB", localStorage),
+			db: new localStorageDB(_DB, localStorage),
 			geocoder: new google.maps.Geocoder(),
 			map: false,
 			mapDiv: $mapDiv,
 			mapOptions: {
 				center: new google.maps.LatLng(39.76, -86.15), 
-				mapTypeId: google.maps.MapTypeId.ROADMAP, 
+				mapTypeId: google.maps.MapTypeId.HYBRID, 
 				scrollwheel: false,
 				zoom: 8,
 			},
 			markers: [],
 		}
 		
-		t.init = function(options) {
+		//merge options into object
+		if(options) {
+			t = $.extend(true, t, options);
+		}
+		
+		t.init = function(options) {		
 			if(options) {
 				t.mapOptions = $.extend(true, t.mapOptions, options);	
 			}		
 			t.map = new google.maps.Map(t.mapDiv[0], t.mapOptions);
+			
+			if(!t.db.tableExists(_TABLE)) {
+				t.db.createTable(_TABLE, ["name", "street", "city", "state", "zip", "lat", "lng", /*"marker" */]);
+				t.db.commit();
+			} else {
+				t.db.query(_TABLE, function(row){
+					t.addMarker(row.lat, row.lng, row.name);
+				});
+			}
+			
+			if(t.markers.length>1) {
+				t.map.fitBounds(t.bounds);
+			}
+			
 			return t.map;
 		}
 		
@@ -35,19 +56,32 @@
 				title: name,
 			});
 			
-			t.markers.push(marker);
-			
-			if(!(t.bounds.contains(latlng))) {
-				t.bounds.extend(latlng);
-				t.map.fitBounds(t.bounds);
-			}
-			
+			t.markers.push(marker);			
+			t.bounds.extend(latlng);
 			return marker;
+		}
+		
+		t.saveRow = function(row) {
+			if(t.db.tableExists(_TABLE)) {
+				t.db.insert(_TABLE, {
+					name: row.name,
+					street: row.street,
+					city: row.city,
+					state: row.state,
+					zip: row.zip,
+					lat: row.lat,
+					lng: row.lng,
+					//marker: row.marker,
+				});
+				t.db.commit();
+			} else {
+				console.log("table doesnt exist yet");
+			}
 		}
 		
 		t.geocode = function(location, callback) {
 		
-			t.geocoder.geocode({'address': location}, function(results, status) {
+			t.geocoder.geocode({"address": location}, function(results, status) {
 				
 				var response = {
 					success: status == google.maps.GeocoderStatus.OK ? true : false,
