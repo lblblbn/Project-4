fd33834a24e6fd1673da2cbf9932abba1a951622
lb <lb@fake.email>
2013-03-25 20:42:11 -0400

    can add markers to map

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,55 @@
 	var jQT, map;
 	
 	$(document).ready(function() {
-	
+		map = $("#map").gmap();
+		map.init();
+		
 		jQT = $.jQTouch({
 				statusBar: 'black'
 		});
 		
-		map = $("#map").gmap();
+		var goHome = function() {
+			$('a[href="#home"]').click();	
+		}
+		
+		$('#home').bind('pageAnimationEnd', function(event, info) {
+			if (info.direction == 'in') {
+				$("#map").show();				
+				google.maps.event.trigger(map.map, 'resize');
+			}
+			return false;
+		});
+	
+		$("#new-marker").submit(function(e) {
+			var $t      = $(this);
+			var $name   = $t.find("#name");
+			var $street = $t.find("#street");
+			var $city   = $t.find("#city");
+			var $state  = $t.find("#state");
+			var $zip    = $t.find("#zip");
+			
+			var address = [
+				$street.val(),
+				$city.val(),
+				$state.val(),
+				$zip.val()
+			];
+			
+			map.geocode(address.join(" "), function(response) {
+				console.log(response);
+				
+				if(response.success) {	
+					var lat = response.results[0].geometry.location.lat();
+					var lng = response.results[0].geometry.location.lng();
+					
+					map.addMarker(lat, lng, $name.val());
+				}
+			});
+			
+			goHome();
+			e.preventDefault();
+		});
+
 		
 	});
 }(jQuery, this));
--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -4,21 +4,62 @@
 		var $mapDiv = $(obj);
 	
 		var t = {
+			bounds: new google.maps.LatLngBounds(),
+			//db: new localStorageDB("markerDB", localStorage),
+			geocoder: new google.maps.Geocoder(),
+			map: false,
 			mapDiv: $mapDiv,
 			mapOptions: {
 				center: new google.maps.LatLng(39.76, -86.15), 
 				mapTypeId: google.maps.MapTypeId.ROADMAP, 
-				scrollwheel: true,
+				scrollwheel: false,
 				zoom: 8,
 			},
+			markers: [],
 		}
 		
 		t.init = function(options) {
+			if(options) {
+				t.mapOptions = $.extend(true, t.mapOptions, options);	
+			}		
 			t.map = new google.maps.Map(t.mapDiv[0], t.mapOptions);
 			return t.map;
 		}
 		
-		t.init();
+		t.addMarker = function(lat, lng, name) {
+			var latlng = new google.maps.LatLng(lat, lng);
+		
+			var marker = new google.maps.Marker({
+				map: t.map,
+				position: latlng,
+				title: name,
+			});
+			
+			t.markers.push(marker);
+			
+			if(!(t.bounds.contains(latlng))) {
+				t.bounds.extend(latlng);
+				t.map.fitBounds(t.bounds);
+			}
+			
+			return marker;
+		}
+		
+		t.geocode = function(location, callback) {
+		
+			t.geocoder.geocode({'address': location}, function(results, status) {
+				
+				var response = {
+					success: status == google.maps.GeocoderStatus.OK ? true : false,
+					status: status,
+					results: results
+				}
+				if(typeof callback === "function") {
+					callback(response);
+				}				
+			});
+		}
+		
 		return t;
 	}
 	
